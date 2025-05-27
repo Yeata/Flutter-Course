f5107e8bab38b8b0270dda9bb180b4f0f56ce214
Yeata <yousyeata1@gmail.com>
2025-05-27 22:09:59 +0700

    Landing page flutter

---
+++ android/app/src/main/kotlin/com/example/tp04/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.tp04
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'pages/landing_page.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Worktency',
+      theme: ThemeData(primarySwatch: Colors.orange),
+      home: LandingPage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
